9414d6e04edb9603b50bfe52f7fcbfae3b5cae92
Joey <joseph.ray@workpac.com>
2021-01-31 04:43:13 -0800

    simple optimizations of problem 9

--- Problem9.cpp
+++ Problem9.cpp
@@ -13,30 +13,46 @@ int check_triplet(int a, int b, int c)
 	return ((a * a) + (b * b) == (c * c));
 }
 
+// we assert a, b, c are all less than 500.
+// if a > 500 then with b > a any sum is > 1000
+// if b > 500 then with c > b any sum is > 1000
+// if c > 500 then the square is too big to be pythag triplet
 int problem9()
 {
 	for (int a = 2; a < 500; a++)
 	{
-		for (int b = a + 1; b < 500; b++)
+		for (int b = 500 - a; b < 500; b++)
 		{
-			for (int c = b + 1; c < 500; c++)
+			iter9++;
+			
+			// does a have any chance of forming triplet
+			if ((b + 1) * (b + 1) - (b * b) > a * a)
 			{
-				iter9++;
-				if (a * a + b * b < c * c)
-				{
-					break;
-				}
-				if (check_triplet(a, b, c) && a + b + c == 1000)
-				{
-					return a * b * c;
-				}
+				break;
+			}
+
+			int c = 1000 - a - b; // only value c can be when b is set
+			if (c <= b)
+			{
+				break;
+			}
+
+			if (check_triplet(a, b, c))
+			{
+				return a * b * c;
 			}
 		}
 	}
 	throw 1;
 }
 
+// iter9 values:
 // naieve = 99,575,750
-// 25,155,864
-// 1,584,490
-// 1,549,959
+// v1: 25,155,864
+// v2: 1,584,490
+// v3: 1,549,959
+// v4: 1,540,205
+// v5: 869,660
+// v6: 69,294
+// v7: 59,832
+// v8: 10,030
